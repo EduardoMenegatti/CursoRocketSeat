7d00fe7307146bf760e944f5a37922af3c542470
EduardoMenegatti <emmenegatti@gmail.com>
2021-02-08 14:52:25 -0300

    created file data

---
+++ modulo3/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+      id: "uFbE3O-eQsM",
+      title: "Adoração infantil | Jacqueline G." ,
+      duration: "3:26",
+      price: "Free" 
+    },
+    {
+        id: "uFbE3O-eQsM",
+        title: "Adoração infantil | Jacqueline G." ,
+        duration: "3:26",
+        price: "Free" 
+    },
+    {
+        id: "uFbE3O-eQsM",
+        title: "Adoração infantil | Jacqueline G." ,
+        duration: "3:26",
+        price: "Free" 
+    },
+    {
+        id: "uFbE3O-eQsM",
+        title: "Adoração infantil | Jacqueline G." ,
+        duration: "3:26",
+        price: "Free" 
+    },
+    {
+        id: "uFbE3O-eQsM",
+        title: "Adoração infantil | Jacqueline G." ,
+        duration: "3:26",
+        price: "Free" 
+    },
+    {
+        id: "uFbE3O-eQsM",
+        title: "Adoração infantil | Jacqueline G." ,
+        duration: "3:26",
+        price: "Free" 
+    }
+]
