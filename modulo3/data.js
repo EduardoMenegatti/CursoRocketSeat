3f76d42325235673969b0f5a3d3ccb5b9ea87071
EduardoMenegatti <emmenegatti@gmail.com>
2021-02-11 20:32:09 -0300

    start server desafio.2,3

--- modulo3/data.js
+++ modulo3/data.js
@@ -7,7 +7,7 @@ module.exports = [
     },
     {
         id: "uFbE3O-eQsM",
-        title: "Adoração infantil | Jacqueline G." ,
+        title: "Adoração infantil |Jac" ,
         duration: "3:26",
         price: "Free" 
     },
@@ -25,13 +25,13 @@ module.exports = [
     },
     {
         id: "uFbE3O-eQsM",
-        title: "Adoração infantil | Jacqueline G." ,
+        title: "Adoração infantil escola 234 | Jacqueline G." ,
         duration: "3:26",
         price: "Free" 
     },
     {
         id: "uFbE3O-eQsM",
-        title: "Adoração infantil | Jacqueline G." ,
+        title: "Adoração infantil  esta equipe esta testando com titulo mairo| Jacqueline G." ,
         duration: "3:26",
         price: "Free" 
     }
--- modulo3/server.js
+++ modulo3/server.js
@@ -2,16 +2,35 @@ const express = require('express')
 const nunjuncks = require('nunjucks')
 
 const server = express()
+const videos = require("./data")
 
-server.set("view engine", "html")
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
 nunjuncks.configure("views", {
-    express:server
+    express: server
 })
 
-server.get("/", function(req, res){
-    return res.render("index")
+server.get("/", function (req, res) {
+    const data = {
+        avatar_url: "https://media-exp1.licdn.com/dms/image/C4D03AQFdyxT51d8UuQ/profile-displayphoto-shrink_200_200/0/1589457909659?e=1618444800&v=beta&t=gpYbgMvbByRjH9kp74b-dobI2z70Zzacz1vgzS9IV50",
+        name: "Jacqueline M. G. Marques",
+        role:"Cientista de Alimentos",
+        description: "Focada em gerenciamento e produção de alimentos com as boas práticas",
+        links:[
+            {name:"Curriculo Lattes", url:"http://buscatextual.cnpq.br/buscatextual/visualizacv.do?metodo=apresentar&id=K4223048U7"},
+            {name:"Instagram", url:"https://www.instagram.com/jacquelinegerageconsultoria/?hl=pt-br"},
+            {name:"Linkedin", url:"https://www.linkedin.com/in/jacqueline-gerage-b7305356/"},
+            {name: "ESALQ", url: "https://www.esalq.usp.br"},
+        ]
+    }
+    return res.render("about",{about:data})
 })
 
-server.listen(5000, function(){
+server.get("/portfolio", function (req, res) {
+    return res.render("portfolio",{items:videos})
+})
+
+server.listen(5000, function () {
     console.log("server is running")
 })
